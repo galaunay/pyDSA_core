1fde4e80fe60458ac72f67f255ed899401778d29
galaunay <gaby.launay@tutanota.com>
2018-08-17 23:40:48 +0100

    Add tests on Image

--- pyDSA/image.py
+++ pyDSA/image.py
@@ -237,12 +237,18 @@ class Image(ScalarField):
         scalex, scaley, scalev : numbers or Unum objects
             Scale for the axis and the values
         """
-        super().scale(scalex=scalex, scaley=scaley, scalev=scalev,
-                      inplace=inplace)
-        if self.baseline is not None:
-            self.baseline.scale(scalex=scalex, scaley=scaley)
-        if self.cache_infos:
-            self._dump_infos()
+        if inplace:
+            super().scale(scalex=scalex, scaley=scaley, scalev=scalev,
+                          inplace=True)
+            tmpim = self
+        else:
+            tmpim = super().scale(scalex=scalex, scaley=scaley, scalev=scalev,
+                                  inplace=False)
+        if tmpim.baseline is not None:
+            tmpim.baseline.scale(scalex=scalex, scaley=scaley)
+        if tmpim.cache_infos:
+            tmpim._dump_infos()
+        return tmpim
 
     def _dump_infos(self):
         # Gather old information if necessary
@@ -300,184 +306,6 @@ class Image(ScalarField):
         """
         sc = TriplePointChooser(self)
         return sc.rh
-
-    def binarize(self, method='adaptative', threshold=None, inplace=False):
-        """
-        Binarize the image.
-
-        Parameters
-        ==========
-        method: string in ['adaptative', 'simple', 'otsu']
-            Method used to binarize (see opencv documention)
-        threshold: number
-            Threshold for the 'simple' method
-        inplace: boolean
-            To binarize in place or not.
-
-        Returns:
-        ========
-        new_img: Image object
-            Binarized image
-        """
-        if method == 'simple':
-            pass
-        elif method == 'adaptative':
-            pass
-        elif method == 'otsu':
-            pass
-        else:
-            raise ValueError
-
-    def _binarize_simple(self, threshold, inplace=False):
-        """
-        Binarize the image using a threshold.
-
-        Parameters
-        ==========
-        threshold: number
-            Threshold.
-        inplace: boolean
-            To binarize in place or not.
-
-        Returns:
-        ========
-        new_img: Image object
-            Binarized image
-        """
-        if inplace:
-            tmp_im = self
-        else:
-            tmp_im = self.copy()
-        ret_val, new_img = cv2.threshold(tmp_im.data,
-                                         threshold,
-                                         255,
-                                         cv2.THRESH_BINARY)
-        tmp_im.data = new_img
-        tmp_im.used_threshold = int(ret_val)
-        return tmp_im
-
-    def _binarize_adaptative(self, kind='gaussian', size=101, inplace=False):
-        """
-        Binarize the image using a threshold.
-
-        Parameters
-        ==========
-        kind: string in ['gaussian', 'mean']
-            Type of threshold computation.
-        size: number
-            Size of the area used to binarize. Should be odd.
-        threshold: number
-            Threshold.
-        inplace: boolean
-            To binarize in place or not.
-
-        Returns:
-        ========
-        new_img: Image object
-            Binarized image
-        """
-        # checks
-        if kind == 'gaussian':
-            kind = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
-        elif kind == 'mean':
-            kind = cv2.ADAPTIVE_THRESH_MEAN_C
-        else:
-            raise ValueError
-        if size <= 0:
-            raise ValueError
-        if size % 2 != 1:
-            size += 1
-            warnings.warn(f"size should be odd and has been set to {size}" +
-                          f" instead of {size - 1}")
-        #
-        if inplace:
-            tmp_im = self
-        else:
-            tmp_im = self.copy()
-        new_img = cv2.adaptiveThreshold(tmp_im.data,
-                                        255,
-                                        kind,
-                                        cv2.THRESH_BINARY,
-                                        size,
-                                        1)
-        tmp_im.data = new_img
-        tmp_im.used_threshold = 'adapt'
-        return tmp_im
-
-    def _binarize_otsu(self, inplace=True):
-        """
-        Binarize the image using a threshold choosen using the image histogram.
-
-        Parameters
-        ==========
-        inplace: boolean
-            To binarize in place or not.
-
-        Returns:
-        ========
-        new_img: Image object
-            Binarized image
-        """
-        if inplace:
-            tmp_im = self
-        else:
-            tmp_im = self.copy()
-        ret_val, new_img = cv2.threshold(self.data,
-                                         0,
-                                         255,
-                                         cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        tmp_im.data = new_img
-        tmp_im.used_threshold = int(ret_val)
-        return tmp_im
-
-    def fill_holes(self, size=2, iterations=1, inplace=False):
-        """
-        Fill the holes in the image.
-
-        Parameters
-        ==========
-        size: integer
-            Size of the hole to fill.
-        iterations: integet
-            Number of iterations
-
-        Returns
-        =======
-        tmp_im : Image object
-            Transformation of the initial image where holes have been filled.
-        """
-        raise Exception('Obsolete')
-        if inplace:
-            tmp_im = self
-        else:
-            tmp_im = self.copy()
-        # first, remove holes
-        tmp_im.data = spim.binary_closing(tmp_im.data,
-                                          structure=np.ones((size, size)),
-                                          iterations=iterations)
-        # remove black border
-        border = size + iterations
-        tmp_im.data = tmp_im.data[border:-border, border:-border]
-        # second, only keep biggest white area
-        labels, nmb = spim.label(tmp_im.data)
-        sizes = [np.sum(labels == label) for label in np.arange(1, nmb + 1)]
-        bigger_label = np.argmax(sizes) + 1
-        labels[labels != bigger_label] = 0
-        tmp_im.data = labels
-        # third, only keep biggest black area
-        tmp_data = tmp_im.data.copy()
-        tmp_data[tmp_im.data == 1] = 0
-        tmp_data[tmp_im.data == 0] = 1
-        labels, nmb = spim.label(tmp_data)
-        sizes = [np.sum(labels == label) for label in np.arange(1, nmb + 1)]
-        bigger_label = np.argmax(sizes) + 1
-        labels[labels != bigger_label] = 1
-        # store
-        tmp_data = labels.copy()
-        tmp_data[labels == 1] = 0
-        tmp_data[labels == 0] = 1
-        tmp_im.data = tmp_data
-        return tmp_im
 
     def edge_detection_canny(self, threshold1=None, threshold2=None,
                              base_max_dist=15, size_ratio=.5,
@@ -642,7 +470,7 @@ class Image(ScalarField):
                 plt.title('Removed small edges')
             # Remove if not touching the baseline
             if nmb_edge > nmb_edges:
-                for i in range(nmb_edge):
+                for i in range(np.max(labels)):
                     ys = Y[labels == i+1]
                     if len(ys) == 0:
                         continue
@@ -774,50 +602,6 @@ class Image(ScalarField):
         return edge
 
     edge_detection = edge_detection_canny
-
-    def circle_detection(self, dp=1., minDist=10, verbose=False):
-        """
-        Detect the circle present in the image.
-
-        Parameters
-        ==========
-        dp: number
-            Inverse ratio of the accumulator resolution.
-            (Put a bigger number to get more circles !)
-        minDist: number
-            Minimum distance between two circle centers.
-
-        Notes
-        =====
-        Use the opencv HoughCircle function.
-        """
-        values = np.array(self.values, dtype=np.uint8)
-        circles = cv2.HoughCircles(values,
-                                   cv2.HOUGH_GRADIENT,
-                                   dp=dp,
-                                   minDist=minDist)[0]
-        # Adapt to current axes
-        print(circles)
-        circles = np.array(circles)
-        tmp = circles[:, 0].copy()
-        circles[:, 0] = circles[:, 1]*self.dx
-        circles[:, 1] = tmp*self.dy
-        circles[:, 2] *= (self.dy + self.dx)/2
-        print(self.dx, self.dy)
-        print(circles)
-        # display if asked
-        if verbose:
-            from matplotlib.collections import PatchCollection
-            from matplotlib.patches import Wedge
-            plt.figure()
-            self.display()
-            patches = []
-            for (x, y, r) in circles:
-                patches.append(Wedge((x, y), r, 0, 360, width=self.axe_x[-1]*.001))
-                plt.plot(x, y, "ok")
-            p = PatchCollection(patches)
-            plt.gca().add_collection(p)
-            plt.show()
 
 
 class ScaleChooser(object):
---
+++ tests/test_image.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+#!/bin/env python3
+
+# Copyright (C) 2018 Gaby Launay
+
+# Author: Gaby Launay  <gaby.launay@tutanota.com>
+# URL: https://github.com/gabylaunay/pyDSA
+# Version: 0.1
+
+# This file is part of pyDSA
+
+# pyDSA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+import os
+import sys
+try:
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dirname)
+    os.chdir('..')
+    sys.path.append(os.getcwd())
+except:
+    pass
+
+import numpy as np
+from helper import sane_parameters
+from pyDSA.helpers import import_from_image, import_from_images, \
+    import_from_video
+from pyDSA import import_from_image
+from IMTreatment.utils import make_unit
+import json
+import pytest
+
+
+class TestImportFromImage(object):
+    """ Done """
+
+    def setup(self):
+        sane_parameters()
+        self.im = import_from_image("./tests/res/image.bmp",
+                                    dx=0.12, dy=0.12,
+                                    unit_x="mm",
+                                    unit_y="um",
+                                    cache_infos=False,
+                                    dtype=np.uint8)
+        self.basept1 = np.array([13, 27.3*12/14])
+        self.basept2 = np.array([83.88, 26.88*12/14])
+        self.im.set_baseline(self.basept1, self.basept2)
+
+    def test_eq(self):
+        im2 = self.im.copy()
+        im3 = self.im.copy()
+        assert im2 == self.im
+        im2.values[10, 10] = 9999
+        assert im2 != self.im
+        im3.baseline.pt1 = [3, 4]
+        assert im3 != self.im
+
+    def test_set_baseline(self):
+        im2 = self.im.copy()
+        im2.set_baseline([0, 4], [im2.axe_x[-1], 5])
+        assert np.allclose(im2.baseline.pt1, [0, 4])
+        assert np.allclose(im2.baseline.pt2, [im2.axe_x[-1], 5])
+
+    def test_scale(self):
+        im2 = self.im.copy()
+        assert im2 == self.im
+        im3 = im2.scale(scalex=2, scaley=0.2, scalev=3, inplace=False)
+        assert im2 == self.im
+        assert np.isclose(im3.dx, 2*im2.dx)
+        assert np.isclose(im3.dy, 0.2*im2.dy)
+        assert np.isclose(im3.max, 255)
+        assert np.isclose(im3.min, im2.min*3)
+        assert np.allclose(im3.baseline.pt1, im2.baseline.pt1*[2, 0.2])
+        assert np.allclose(im3.baseline.pt2, im2.baseline.pt2*[2, 0.2])
+
+    def test_infofile(self):
+        im = import_from_image("./tests/res/image.bmp",
+                               dx=0.12, dy=5.14,
+                               unit_x="mm",
+                               unit_y="um",
+                               cache_infos=True,
+                               dtype=np.uint8)
+        im.set_baseline(self.basept1, self.basept2)
+        # Check the baseline is recorded
+        with open(im.infofile_path, 'r') as f:
+            infos = json.load(f)
+        assert np.allclose(infos['baseline_pt1'], im.baseline.pt1)
+        assert np.allclose(infos['baseline_pt2'], im.baseline.pt2)
+        # check the scaling is recorded
+        dx = im.dx.copy()
+        dy = im.dy.copy()
+        base1 = im.baseline.pt1.copy()
+        base2 = im.baseline.pt2.copy()
+        im.scale(scalex=2, scaley=0.2, inplace=True)
+        with open(im.infofile_path, 'r') as f:
+            infos = json.load(f)
+        assert np.allclose(infos['baseline_pt1'], base1*[2, 0.2])
+        assert np.allclose(infos['baseline_pt2'], base2*[2, 0.2])
+        assert infos['dx'] == dx*2
+        assert infos['dy'] == dy*0.2
+        os.remove(im.infofile_path)
+
+    def test_edge_detection_canny(self):
+        # nothing without baseline
+        im2 = self.im.copy()
+        im2.baseline = None
+        with pytest.raises(Exception):
+            im2.edge_detection_canny()
+        # test1
+        edge = self.im.edge_detection_canny(nmb_edges=1)
+        assert np.isclose(np.max(edge.xy[:, 1]), 36.36)
+        # test2
+        edge2 = self.im.edge_detection_canny(
+            threshold1=100,
+            threshold2=200,
+            base_max_dist=0,
+            size_ratio=.2,
+            nmb_edges=2,
+            smooth_size=4)
+        # test3
+        with pytest.raises(Exception):
+            edge2 = self.im.edge_detection_canny(
+                threshold1=100,
+                threshold2=200,
+                base_max_dist=0,
+                size_ratio=.2,
+                nmb_edges=1,
+                smooth_size=4)
+
+    def test_edge_detection_contour(self):
+        # test1
+        edge = self.im.edge_detection_contour(
+            nmb_edges=2,
+            ignored_pixels=2,
+            level=0.75,
+            size_ratio=0.5)
+        assert np.isclose(np.max(edge.xy[:, 1]), 36.46894736)
+        # test2
+        edge2 = self.im.edge_detection_contour(
+            nmb_edges=1,
+            ignored_pixels=2,
+            level=0.25,
+            size_ratio=0.5)
+        assert np.isclose(np.max(edge2.xy[:, 1]), 36.211499999)
