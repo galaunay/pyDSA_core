1f480af3afd2f81eb2546c8c2c625b4a952a8260
galaunay <gaby.launay@tutanota.com>
2019-04-07 03:58:53 +0100

    Clean upload to pypi

--- pypi-release.sh
+++ pypi-release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# bumpversion minor
 sudo python3 -m pip install --upgrade setuptools wheel twine
 sudo rm -rf dist
 sudo python3 setup.py sdist bdist_wheel
 twine upload dist/*
+# git push
+# git push --tags
