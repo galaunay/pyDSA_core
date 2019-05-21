06d5f5301c8d7664325685ca90f2d87040a83966
galaunay <gaby.launay@tutanota.com>
2019-05-21 19:38:07 +0100

    Updated release script

--- pypi-release.sh
+++ pypi-release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# bumpversion minor
+bumpversion minor
 sudo python3 -m pip install --upgrade setuptools wheel twine
 sudo rm -rf dist
 sudo python3 setup.py sdist bdist_wheel
 twine upload dist/*
-# git push
-# git push --tags
+git push
+git push --tags
